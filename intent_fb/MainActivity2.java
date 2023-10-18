abb76a4684685e90485163acbd61d6197d4f528a
lucifer <lucifer13@gmail.com>
2023-10-18 11:25:20 +0530

    initial

---
+++ intent_fb/MainActivity2.java
@@ -0,0 +1,21 @@
+package com.example.facebook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+    TextView t1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        t1=(TextView)findViewById(R.id.tv);
+        Intent intent=getIntent();
+        String str=intent.getStringExtra("username");
+        t1.setText(str);
+
+    }
+}
